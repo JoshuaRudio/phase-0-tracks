f7fd61a456c1b7fb747316b9b0ecc70c30ceb0f0
Joshua Rudio <joshuarudio@gmail.com>
2017-04-21 16:36:24 -0700

    Added ability to process multiple employees, ability to check for suspicious allergies, and the plot twist

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,12 @@
+employees = " "
+line = " "
+
 puts "Hello, welcome to Werewolf Inc. Where you'll be howling at the moon with so much fun you'll be having."
+puts "How many employees are to be processed?"
+employees = gets.chomp.to_i
+
+while employees > 0
+
 puts "What is your name?"
 name = gets.chomp
 puts "Alright #{name}, how old will you be at the end of this year?"
@@ -9,6 +17,18 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you? (
 garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance? ( Yes / No )"
 insurance = gets.chomp
+
+while line != "Done"
+	line = " "
+	puts "Please list your allergies down one at a time. When you are finished, type in 'Done'."
+	line = gets.chomp
+	if line == "Sunshine"
+		dislikes_sun = true
+		break
+	end
+end
+
+employees -= 1
 
 if garlic_bread == "Yes"
 	likes_garlic_bread = true
@@ -29,14 +49,24 @@ if name == "Drake Cula" || name == "Tu Fang"
 	vampire_name = true
 end
 
-if vampire_name
+
+if dislikes_sun
+	puts "#{name} is PROBABLY a vampire..."		
+elsif vampire_name
 	puts "#{name} IS DEFINITELY A VAMPIRE!"
 elsif !real_age && !likes_garlic_bread && !wants_insurace 
 	puts "#{name} is almost certainly a vampire!"
-elsif !real_age && ( likes_garlic_bread || wants_insurace)
+elsif !real_age && !( likes_garlic_bread || wants_insurace )
 	puts "#{name} is PROBABLY a vampire..."		
 elsif real_age && likes_garlic_bread && wants_insurace
 	puts "#{name} is probably NOT a vampire!"
 else
 	puts "Results inconclusive..."
 end
+
+line = " "
+dislikes_sun = false
+
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."

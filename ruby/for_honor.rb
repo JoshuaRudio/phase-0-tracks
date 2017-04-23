bd1880efc5e2015ed712e62149fa8b2eac4579da
Joshua Rudio <joshuarudio@gmail.com>
2017-04-22 21:58:59 -0700

    created for_honor and psuedocode

---
+++ ruby/for_honor.rb
@@ -0,0 +1,9 @@
+=begin
+Knights, Vikings, or Samurai?
+
+1. Ask questions about prefered weapon.
+2. Ask questions about honor.
+3. Ask about gear.
+4. Set conditions and decide if Knight, Samurai, or Vikings.
+5. Display result.
+=end

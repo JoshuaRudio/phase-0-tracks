d33acfa073356f6eee75ac497cd3d0706fe2b34c
Joshua Rudio <joshuarudio@gmail.com>
2017-04-26 21:19:30 -0700

    Added release 0 and release 1

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,76 @@
+# Release 0
+
+=begin
+test_array = [33, 85, 27, 90, 15, 3] 
+
+number = 0
+
+def search_array (array_name, number)
+
+loopcounter = 0
+array_index = 0
+easy_out_clause = 0
+
+until easy_out_clause == true
+ 
+ array_name.each do |value|
+
+  case
+      when value == number
+        
+        loopcounter += 1
+        easy_out_clause = true
+        array_index = loopcounter - 1
+        break
+      else
+        loopcounter += 1
+  end #case end
+
+  easy_out_clause = true
+  array_index = nil
+  
+  end #each end
+
+end #until end
+
+array_index
+
+end #method end
+ 
+p search_array(test_array,3)
+=end
+
+# Release 1 Calculate Fibonacci Numbers
+
+=begin
+fib(num)
+while loopcount < num
+	if loopcount == 0
+		fib.arr << 0
+	if loopcount == 1
+		fib.arr << 1
+	else
+		next_num = fib.arr [index-1] + [index-2]
+fib.arr << next_num
+loopcount += 1
+=end
+
+def fib(x)
+	loopcount = 0
+	fib_array = []
+	next_num = 0
+	while loopcount < x
+		if loopcount == 0
+			fib_array << 0
+		elsif loopcount == 1
+			fib_array << 1
+		else loopcount >= 1
+			next_num = fib_array[-1] + fib_array[-2]
+			fib_array << next_num
+		end
+	loopcount += 1
+end
+return fib_array
+end
+p fib(6)
+p fib(100)

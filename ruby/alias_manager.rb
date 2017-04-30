7a3c8f0175659c03fd72d0bbfd1442035af28733
Joshua Rudio <joshuarudio@gmail.com>
2017-04-29 17:55:12 -0700

    Added a pseudocode for release 0

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,9 @@
+# Release 0
+=begin
+The goal is to take a spy's name and create a fake name by:
+Switching the first and last name, probably by using split(" ")
+Then change all the vowels in the name to the next vowel.
+1. Get full name by asking user for their first and last name.
+2. Create method that will first swap the first and last name, and then sequence each vowel to the next vowel in the order of 'AEIOU'
+3. Puts the new alias
+=end

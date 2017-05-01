b72b871dffa7a19b11e11c93cce5e0f13c9f994e
Joshua Rudio <joshuarudio@gmail.com>
2017-04-30 17:53:11 -0700

    Added a vowel changer

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -16,5 +16,25 @@ def name_splitter(full_name)
 	names = names[1] + " " + names[0]
 end
 
+
+def vowel_changer(full_name)
+
+upper_vowels = ['A', 'E', 'I', 'O', 'U', 'A']
+lower_vowels = ['a', 'e', 'i', 'o', 'u', 'a']
+
+name = full_name.split('')
+
+name.map! do |char, index|
+	if lower_vowels.include? char
+		char = lower_vowels[lower_vowels.index(char) + 1 ]
+	elsif upper_vowels.include? char
+		char = upper_vowels[upper_vowels.index(char) + 1 ]
+	else
+		char
+	end
+end
+altered_name = name.join('')
+end
+
 p name_splitter(full_name)
-p vowels
+p vowel_changer(full_name)

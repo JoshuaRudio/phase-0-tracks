edcb66e3b5892a4429233ea14ff1fa98904558bc
Joshua Rudio <joshuarudio@gmail.com>
2017-05-01 16:21:45 -0700

    designed a puppy class and declared puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+=begin
+SPECIES ------------
+Red-Cliff Gigantus
+
+CHARACTERISTICS ----
+Fur color: red
+Size: xxxl
+Weight: varies
+Friendly: yes
+Indoor dog: no
+
+BEHAVIOR -----------
+Lick
+Bark
+Dig
+Growl
+Fetch
+=end
+
+class Puppy
+	end
+
+p Puppy.methods

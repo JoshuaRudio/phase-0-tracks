0beb3b765f27ab618ac5735aedef0cd78e54c28d
Joshua Rudio <joshuarudio@gmail.com>
2017-05-01 16:34:23 -0700

    did the things for releases 1 and 2

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -20,4 +20,15 @@ Fetch
 class Puppy
 	end
 
-p Puppy.methods
+#p Puppy.methods
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+clifford = Puppy.new
+
+#p spot.class
+#p duchess == fido
+#p fido.instance_of?(Array)
+#p fido.instance_of?(Puppy)
+#p spot.play_dead

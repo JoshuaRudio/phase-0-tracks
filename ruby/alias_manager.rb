65b9e1290ae77430574485ff89b4687f1a4ab3bd
Joshua Rudio <joshuarudio@gmail.com>
2017-04-30 19:03:53 -0700

    Added a basic user interface to program

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,75 +8,17 @@ Then change all the vowels in the name to the next vowel.
 2. Create method that will first swap the first and last name, and then sequence each vowel to the next vowel in the order of 'AEIOU'
 3. Puts the new alias
 =end
-=begin
-full_name = "Joshua Rudio"
-vowels = ["a", "e", "i", "o", "u"]
-
-def 	name_splitter(full_name)
-	names = full_name.split(" ")
-	names = names[1] + " " + names[0]
-end
-
-
-def vowel_changer(full_name)
-
-upper_vowels = ['A', 'E', 'I', 'O', 'U', 'A', '-', '-']
-lower_vowels = ['a', 'e', 'i', 'o', 'u', 'a', '-', '-']
-
-name = full_name.split('')
-
-name.map! do |char, index|
-	if lower_vowels.include? char
-		char = lower_vowels[lower_vowels.index(char) + 1 ]
-	elsif upper_vowels.include? char
-		char = upper_vowels[upper_vowels.index(char) + 1 ]
-	else
-		char
-	end
-end
-altered_name = name.join('')
-end
-
-#p name_splitter(full_name)
-#p vowel_changer(full_name)
-
-def consonant_changer(full_name)
-
-upper_cons = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'B', '-', '-']
-lower_cons = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'b']
-
-name = full_name.split('')
-
-name.map! do |char, index|
-	if lower_cons.include? char
-		char = lower_cons[lower_cons.index(char) + 1 ]
-	elsif upper_cons.include? char
-		char = upper_cons[upper_cons.index(char) + 1 ]
-	else
-		char
-	end
-end
-altered_name = name.join('')
-end
-
-#p consonant_changer(full_name)
-
-def alias_manager(full_name)
-	step_0 = full_name.name_splitter
-	step_1 = swapped.vowel_changer
-	step_2 = devoweled.consonant_changer
-	p step_2
-end
-
-alias_manager(full_name)
-=end
-
-full_name = "Joshua Rudio"
 
 def alias_maker(full_name)
-  
+=begin
 names = full_name.split(" ")
-swapped = names[1] + " " + names[0]
+swapped = names[1] + names[0]
+=end
+if full_name.split(" ").length > 1
+	names = full_name.split(" ")
+else
+	full_name
+end
 	
 upper_vowels = ['A', 'E', 'I', 'O', 'U', 'A']
 lower_vowels = ['a', 'e', 'i', 'o', 'u', 'a']
@@ -116,5 +58,17 @@ puts the_alias
 
 end
   
-alias_maker(full_name)
-alias_maker("Felicia Torres")
+# Release 1
+
+puts "Welcome to my alias maker. Continually enter the name(s) you would like to incognitize. When finished, type 'quit.'"
+input = " "
+while input = gets.chomp
+	if input.split(" ").length > 1
+		alias_maker(input) unless input == "quit" || input == "Quit"
+	else
+		puts "This alias maker requires at least a first and last name." unless input == "quit" || input == "Quit"
+	end
+	puts "Enter name:" unless input == "quit" || input == "Quit"
+	break if input == "quit" || input == "Quit"
+end
+puts "Thank you for using my alias maker"

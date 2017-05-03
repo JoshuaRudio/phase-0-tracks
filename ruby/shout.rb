96aa286b0fd37ce8ec9c94a633122c2713b1837a
Joshua Rudio <joshuarudio@gmail.com>
2017-05-02 19:08:02 -0700

    Created a new mixin module and 2 classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,37 @@
+=begin
 module Shout
-	# To be added
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def self.yell_happily(words)
+		words + "!!!" + " :)"
+	end
 end
+
+puts Shout.yell_happily("I'm mad")
+puts Shout.yell_happily("I'm happy")f
+=end
+
+module Shout
+	def happy_shout(words)
+		words + "!!!" + " :)"
+	end
+
+	def mad_shout(words)
+		words.upcase! + "!!!" + " XD"
+	end
+end
+
+class Preachers
+	include Shout
+end
+
+class Ogres
+	include Shout
+end
+
+pastor = Preachers.new
+puts pastor.happy_shout("Praise the Lord")
+shrek = Ogres.new
+puts shrek.mad_shout("Get out of my swamp")

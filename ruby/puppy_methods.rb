9f98ca1c6a20075ac90d9e69408aa4c3e6a5c27c
Joshua Rudio <joshuarudio@gmail.com>
2017-05-02 17:45:13 -0700

    created a Kitten class with 3 methods, made a loop to create 50 instances then iterated array to show methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -35,3 +35,37 @@ daisy.speak(5)
 daisy.roll_over
 puts daisy.dog_years(2)
 daisy.sit
+
+class Kitten
+	def initialize
+		puts "Initializing Kitten instance ..."
+	end
+
+	def meow(int)
+		int.times do
+			puts "meow"
+		end
+	end
+
+	def scratch
+		puts "The cat scratched you"
+	end
+
+	def sleep
+		puts "zzz...zzz...zzz"
+	end
+end
+
+kittens = []
+
+50.times do
+	kittens << Kitten.new
+end
+
+#p kittens
+
+kittens.each do |instance|
+	instance.scratch
+	instance.meow(1)
+	instance.sleep
+end

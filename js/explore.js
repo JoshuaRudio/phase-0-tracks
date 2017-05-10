bbcd33f52cc4053eae4f84f557a49969e607f1dc
Joshua Rudio <joshuarudio@gmail.com>
2017-05-10 15:34:29 -0700

    created explore.js and pseudocode for a method that reverses a string and prints the string

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// Method: to take a string and print the string in reverse order
+// Input: a string
+  // steps:
+  // turn a string into a data structure
+  // reverse the order of the data structure
+  // conver data structure back into string
+// Output: original string in reverse order

22c79c20a4407469d98610278802009dc4f189b4
Joshua Rudio <joshuarudio@gmail.com>
2017-05-10 15:52:02 -0700

    implemented pseudocode into a method

--- js/explore.js
+++ js/explore.js
@@ -5,3 +5,14 @@
   // reverse the order of the data structure
   // conver data structure back into string
 // Output: original string in reverse order
+
+function reverseString(string) {
+	// This will change the string being reversed into an array
+	var arrayOfString = string.split("");
+  // This will reverse the order of the array
+	var reversedArray = arrayOfString.reverse();
+	// This will change the array back into a string with the reversed order
+	var reversedString = reversedArray.join("");
+	// This will return the reversed string for this method
+	return reversedString;
+}

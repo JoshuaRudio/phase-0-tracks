a4e1e40d6fa642fa93222c7ce7b13aa48c528ed8
Joshua Rudio <joshuarudio@gmail.com>
2017-05-20 19:37:59 -0700

    made readme and playlist.rb

---
+++ databases/8.5/playlist.rb
@@ -0,0 +1,9 @@
+# Playlist:
+# This is a program that can:
+  # Add songs to a playlist
+  # Display the playlist
+  # Search for song by artist
+# What the table should look like:
+  # 1|Black Hole Sun|Soundgarden
+  # 2|Car Radio|Twenty One Pilots
+  # 3|Devastated|Joey Bada$$
---
+++ databases/8.5/readme.md
@@ -0,0 +1,2 @@
+# What will I make?
+This will be a short music playlist program that asks a user to add a song to a library of songs.

cc7fa3b46874b46bf66e056d414766231bee1594
Joshua Rudio <joshuarudio@gmail.com>
2017-05-20 20:15:42 -0700

    added method to print the playlist

--- databases/8.5/playlist.rb
+++ databases/8.5/playlist.rb
@@ -7,3 +7,46 @@
   # 1|Black Hole Sun|Soundgarden
   # 2|Car Radio|Twenty One Pilots
   # 3|Devastated|Joey Bada$$
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# create SQLite3 database
+db = SQLite3::Database.new("playlist.db")
+db.results_as_hash = true
+
+# make the table by using string delimiters
+start_playlist = <<-SQL
+	CREATE TABLE IF NOT EXISTS playlist(
+		id INTEGER PRIMARY KEY,
+		title VARCHAR(255),
+		artist VARCHAR(255)
+	);
+SQL
+
+playlist = db.execute("SELECT * FROM playlist")
+
+# make the playlist table (if it's not there already)
+db.execute(start_playlist)
+
+# add a song to the playlist
+def add_song(db, title, artist)
+	db.execute("INSERT INTO playlist (title, artist) VALUES (?, ?)", [title, artist])
+	puts "#{title} by #{artist} was added to your playlist."
+end
+
+def view_playlist(tracks)
+	tracks.each do |song|
+		puts "#{song['title']} by #{song['artist']}"
+	end
+end
+
+#def search_by_artist(artist)
+#	puts "Searching for #{artist} in your playlist"
+#	puts ""
+
+#add_song(db, "Devastated", "Joey Bada$$")
+#puts db.execute("SELECT * FROM playlist")
+
+#view_playlist(playlist)
